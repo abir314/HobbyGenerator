2d5488c2072102c8c016dde7e46d8e3e12ceaca7
abir314 <121821310+abir314@users.noreply.github.com>
2023-05-08 08:10:21 +0200

    Initial commit

---
+++ HobbyGenerator/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Timers;
+using System.Threading;
+
+namespace HobbyGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                HobbyGenerator();
+            }
+        }
+        public static void HobbyGenerator()
+        {
+            Console.WriteLine("Who would like a new hobby?");
+            string yourName = Console.ReadLine();
+            string[] hobbies = new string[5] { "Reading", "League", "Drawing", "Coding", "Nothing" };
+            Random rnd = new Random();
+            int num = rnd.Next(0, hobbies.Length);
+            Console.WriteLine(yourName + " has a hobby of " + hobbies[num]);
+        }
+    }
+}
